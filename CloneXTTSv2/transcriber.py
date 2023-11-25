fbf2cc3c987352ddb52a5420f962dd977b40ca85
IIEleven11 <55998857+IIEleven11@users.noreply.github.com>
2023-11-24 23:27:23 -0800

    Create transcriber.py

---
+++ CloneXTTSv2/transcriber.py
@@ -0,0 +1,57 @@
+import os
+import csv
+from phonemizer import phonemize
+import speech_recognition as sr
+from text import _clean_text
+
+
+def transcribe_audio_files_in_directory(
+    directory_path, txt_file_path, bad_audio_directory
+):
+    r = sr.Recognizer()
+
+    # Create bad audio directory
+    os.makedirs(bad_audio_directory, exist_ok=True)
+
+    with open(txt_file_path, "w", newline="") as txtfile:
+        filenames = sorted(os.listdir(directory_path))
+
+        for filename in filenames:
+            if filename.endswith(".wav"):
+                file_path = os.path.join(directory_path, filename)
+
+                with sr.AudioFile(file_path) as source:
+                    audio = r.record(source)
+                    try:
+                        # transcribe
+                        transcription = r.recognize_google(audio)
+                        # clean the transcription
+                        cleaned_transcription = _clean_text(
+                            transcription, ["english_cleaners2"]
+                        )
+                        # phonemize the cleaned transcription
+                        phonemized_transcription = phonemize(
+                            cleaned_transcription, language="en-us"
+                        )
+                        txtfile.write(f"{filename}|{phonemized_transcription}|1\n")
+                    except sr.UnknownValueError:
+                        print(
+                            f"Google Web Speech API could not understand audio for file: {filename}. Moving it to badAudio directory."
+                        )
+                        # move bad audio badAudio directory
+                        os.rename(
+                            file_path, os.path.join(bad_audio_directory, filename)
+                        )
+                    except sr.RequestError as e:
+                        print(
+                            "Could not request results from Google Web Speech API service; {0}".format(
+                                e
+                            )
+                        )
+
+
+transcribe_audio_files_in_directory(
+    "/home/eleven/segmenter/output",
+    "/home/eleven/segmenter/txtfile/train_data.txt",
+    "/home/eleven/segmenter/badaudio/badaudio",
+)
