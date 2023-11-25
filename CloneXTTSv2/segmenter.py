5ee30cb78d29e17e37cbbe4e9a532e8f28a5f4df
IIEleven11 <55998857+IIEleven11@users.noreply.github.com>
2023-11-24 23:26:14 -0800

    Update segmenter.py

--- CloneXTTSv2/segmenter.py
+++ CloneXTTSv2/segmenter.py
@@ -2,7 +2,7 @@ import auditok
 
 # Load audio file
 audio_regions = auditok.split(
-    "D:\\CloneXTTSv2\\Newdataset48khz11_24.wav",
+    "pathtowav.wav", #path to single wav file
     min_dur=2,  # minimum duration of a valid audio event in seconds
     max_dur=12,  # maximum duration of an event
     max_silence=0.3,  # maximum duration of tolerated continuous silence within an event
@@ -11,4 +11,4 @@ audio_regions = auditok.split(
 
 # Save regions to separate audio files
 for i, region in enumerate(audio_regions):
-    region.save(f"D:\\CloneXTTSv2\\output\\audio{i}.wav")
+    region.save(f"D:\\CloneXTTSv2\\output\\audio{i}.wav") #output folder
